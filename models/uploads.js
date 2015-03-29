27d60cb3690dc830f6519b30de4eb3e98f860ff1
samamaguire <smaguire6@me.com>
2015-03-29 16:19:57 -0400

    s

--- models/uploads.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var UploadSchema = new mongoose.Schema({
-    fileName: String,
-    uploadDate: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Upload', UploadSchema);
